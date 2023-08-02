54323eff38b8eed2336239d3bd0f3da89496738f
Daniel Flores <139706433+DetonatedRAM@users.noreply.github.com>
2023-08-01 19:10:37 -0500

    Delete time.py

--- time.py
+++
@@ -1,8 +0,0 @@
-current_time_Str = input("What is the current time (in hours 0-23)?")
-wait_time_Str = input("How many hours do you want to wait")
-
-currentTimeInt = int(current_time_Str)
-waitTimeInt = int(wait_time_Str)
-
-finalTime_Int = currentTimeInt + waitTimeInt
-print(finalTime_Int)
